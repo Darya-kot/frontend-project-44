ee8947885e59bf477beeaedd45df2e0f892df675
Darya-kot <kot2817220@gmail.com>
2026-04-24 18:20:19 +0300

    Add brain-gcd game: greatest common divisor

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/Darya-kot/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Darya-kot/frontend-project-44/actions)
 [![Quality gate](https://sonarcloud.io/api/project_badges/quality_gate?project=Darya-kot_frontend-project-44)](https://sonarcloud.io/summary/new_code?id=Darya-kot_frontend-project-44)
 [![asciicast](https://asciinema.org/a/iuBvrjJQUTnzeOot)](https://asciinema.org/a/iuBvrjJQUTnzeOot)
+[![asciicast](https://asciinema.org/a/S3opNjJv6nLU3AuD)](https://asciinema.org/a/S3opNjJv6nLU3AuD)
+[![asciicast](https://asciinema.org/a/pSAFShN7o1QYxiPO)](https://asciinema.org/a/pSAFShN7o1QYxiPO)
---
+++ src/games/gcd.js
@@ -0,0 +1,25 @@
+// src/games/gcd.js
+import { runGame } from '../index.js';
+
+// Алгоритм Евклида для нахождения НОД
+const gcd = (a, b) => {
+  if (b === 0) return a;
+  return gcd(b, a % b);
+};
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const generateRound = () => {
+  const num1 = getRandomNumber(1, 100);
+  const num2 = getRandomNumber(1, 100);
+  const correctAnswer = String(gcd(num1, num2));
+  const question = `${num1} ${num2}`;
+  return { question, correctAnswer };
+};
+
+const runGcdGame = () => {
+  const gameDescription = 'Find the greatest common divisor of given numbers.';
+  runGame(gameDescription, generateRound);
+};
+
+export default runGcdGame;
