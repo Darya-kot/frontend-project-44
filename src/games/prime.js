866dfd4b3236bdb874c13dc3210798c1637f4663
Darya-kot <kot2817220@gmail.com>
2026-04-24 19:08:14 +0300

    Add brain-prime game: prime number check

---
+++ src/games/prime.js
@@ -0,0 +1,26 @@
+// src/games/prime.js
+import { runGame } from '../index.js';
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+// Функция-предикат для проверки простоты числа
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const generateRound = () => {
+  const question = getRandomNumber(1, 100);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return { question: String(question), correctAnswer };
+};
+
+const runPrimeGame = () => {
+  const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  runGame(gameDescription, generateRound);
+};
+
+export default runPrimeGame;
